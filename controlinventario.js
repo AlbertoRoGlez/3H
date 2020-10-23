16865966646931bac495b823ab12f92b97621eb8
Alberto Rosales <arosales7@ucol.mx>
2020-10-22 22:27:01 -0500

    commit

--- controlinventario.js
+++ controlinventario.js
@@ -13,5 +13,29 @@ class Producto{
     this.costo=costo
     this.valormerc=(cantidad * costo)
 }
-
 }
+
+let prueba= new Producto("AAAA", "Coca", "Refresco", 2, 15,);
+console.log(prueba);
+
+//Agregar
+var numero = document.querySelector("#agregar1")
+var btnadd = document.querySelector("#btnadd")
+
+//Borrar
+var numero = document.querySelector("#borrar1")
+var btnadd = document.querySelector("#btndelete")
+
+//Buscar
+var numero = document.querySelector("#buscar1")
+var btnadd = document.querySelector("#btnsearch")
+
+//Listar
+var btnadd = document.querySelector("#btnlist")
+
+//Listar Inverso
+var btnadd = document.querySelector("#btninverselist")
+
+//Posición
+var numero = document.querySelector("#posicion1")
+var btnadd = document.querySelector("#btnposition")
