3fdbc2c6a07aba343fdd16c78a965d31474d1975
Alberto Rosales <arosales7@ucol.mx>
2020-11-27 22:21:32 -0600

    commit

--- maincirculares.js
+++ maincirculares.js
@@ -15,7 +15,7 @@ class BasePelican {
     articleToHtml() {
         let productString = '<li class="list-group-item">'
         for (let key in this) {
-            productString += `<div>${key}: ${this[key]}</div>`;
+            productString += `<div>${key}: ${this[key]}</div>`
         }
         return productString + "</li>"
     }
@@ -29,36 +29,120 @@ class PillarOfAutumn {
     agregarPelican(nuevo){
         if (this.inicio===null) {
             this.inicio=nuevo
+            nuevo.siguiente=this.inicio
         }
         else {
             let aux=this.inicio
-            while (aux.siguiente!==null) {
+
+            while (aux.siguiente!==this.inicio) {
                 aux=aux.siguiente
             }
             aux.siguiente=nuevo
+            nuevo.siguiente=this.inicio
         }
         this.tamaño++
-        this.imprimir()
+
     }
     imprimir() {
-        lista.innerHTML = "";
-        let aux = this.inicio;
-        while(aux){
-            lista.innerHTML += aux.articleToHtml();
-            aux = aux.siguiente;
+        lista.innerHTML=""
+        if (!this.inicio) return false
+        let aux=this.inicio
+        let finalizado=false
+        while(!finalizado){
+            lista.innerHTML+=aux.articleToHtml()
+            aux=aux.siguiente
+            if (aux.nombre===this.inicio.nombre) {
+                finalizado=true
+            }
         }
     }
+    buscar(x) {
+        lista.innerHTML=""
+        if (!this.inicio) return false
+        let aux=this.inicio
+        let finalizado=false
+        while (!finalizado) {
+            if (aux.nombre==x) {
+                lista.innerHTML+=aux.articleToHtml()
+                finalizado=true
+                return aux
+            }
+            aux=aux.siguiente
+        }
+        
+    }
+
+    recorrido(horaInicio, baseInicio, horaFinal) {
+        let baseaux=this.inicio
+        console.log(baseaux)
+        let horaaux=Number(horaInicio)
+        console.log(horaaux)
+
+        while (horaaux<horaFinal) {
+            baseaux=baseaux.siguiente
+            horaaux+=Number(baseaux.minutos)
+            console.log(horaaux)
+        }
+    }
+
+    eliminarBase(nombre) {
+        if(this.tamaño === 1){
+         this.inicio = null;
+         this.tamaño--;
+         this.imprimir();
+         return null;
+        }
+         let aux = this.inicio;
+         if(aux.nombre === nombre){
+            this.inicio = aux.siguiente;
+            let temp = this.inicio;
+ 
+             while(temp.siguiente.nombre !== nombre){
+                 temp = temp.siguiente;
+             }
+             this.tamaño--;
+             temp.siguiente = this.inicio;
+             this.imprimir();
+         }
+         else {
+             let temp = this.inicio;
+ 
+             while(temp.siguiente.nombre !== nombre){
+                 temp = temp.siguiente;
+             }
+             temp.siguiente = temp.siguiente.siguiente;
+             this.tamaño--;
+             this.imprimir();
+             return temp.siguiente;
+         }
+ 
+     }
+
 }
 
-let forwardUntoDawn = new PillarOfAutumn();
+let forwardUntoDawn = new PillarOfAutumn()
 
 btnAgregar.addEventListener("click", () => {
-    let nombreBase = (document.querySelector('#nombreBase'));
-    let tiempoBase = (document.querySelector('#tiempoBase'));
-    let base1 = new BasePelican(nombreBase.value, tiempoBase.value);
-    forwardUntoDawn.agregarPelican(base1);
+    let nombreBase = (document.querySelector('#nombreBase'))
+    let tiempoBase = (document.querySelector('#tiempoBase'))
+    let base1 = new BasePelican(nombreBase.value, tiempoBase.value)
+    forwardUntoDawn.agregarPelican(base1)
+    console.log(base1)
 });
 
 btnImprimir.addEventListener("click", () => {
-    forwardUntoDawn.imprimir();
+    forwardUntoDawn.imprimir()
+});
+
+
+btnBuscar.addEventListener("click", () => {
+    let buscar=document.querySelector('#buscarBase')
+    forwardUntoDawn.buscar(buscar.value)
+});
+
+btnCrearRecorrido.addEventListener("click", () => {
+    let baseInicio = document.querySelector("#baseInicio");
+    let horaInicio = document.querySelector("#horaInicio");
+    let horaFinal = document.querySelector("#horaFin");
+    forwardUntoDawn.recorrido(baseInicio.value, horaInicio.value, horaFinal.value);
 });
